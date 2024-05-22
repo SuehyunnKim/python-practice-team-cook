5513dbd8681462195dfb1f6963bb2c7c4af22bf8
SugiyamaKoto <sugiyama.koto.iy@mynavi.jp>
2024-05-22 04:29:29 +0000

    杉山：絶対値を出力

---
+++ Sugiyama/absolute.py
@@ -0,0 +1,15 @@
+#準備
+import sys
+args = sys.argv
+
+#引数を変数に渡す(値の意)
+value = int(args[1])
+
+#整数の変数
+integral_value = value
+
+#絶対値の変数
+absolute_value = abs(value)
+
+#出力
+print(integral_value, absolute_value)
